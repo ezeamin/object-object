9dae85d6472c20c7abc1cfc697b47e59f0d02a52
Ezequiel Amin <ezequielamin@outlook.com>
2023-08-07 12:05:08 -0300

    fix: md editor working after solving issue #561 in package

--- src/components/Create/CreateForm.tsx
+++ src/components/Create/CreateForm.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-// import MDEditor from '@uiw/react-md-editor';
-// import rehypeSanitize from 'rehype-sanitize';
+import MDEditor from '@uiw/react-md-editor';
+import rehypeSanitize from 'rehype-sanitize';
 
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 const CreateForm = () => {
   const { register, handleSubmit: submitRHF } = useForm();
+
+  const [markdown, setMarkdown] = useState('');
+
+  const handleMarkdownChange = (value: string | undefined) => {
+    if (value) setMarkdown(value);
+    else setMarkdown('');
+  };
 
   const handleSubmit = (data: unknown) => console.log(data);
 
@@ -40,17 +48,40 @@ const CreateForm = () => {
           <label htmlFor="content" className="text-lg">
             Content
           </label>
-          {/* <MDEditor
+          <div className="alert mb-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-info shrink-0 w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>
+              You can toggle the preview window with the buttons of the top
+              right corner of the editor.
+            </span>
+          </div>
+          <MDEditor
             id="content"
             className="w-full"
-            value=""
-            onChange={() => {}}
+            value={markdown}
+            onChange={handleMarkdownChange}
             previewOptions={{
               rehypePlugins: [[rehypeSanitize]],
             }}
-            // {...register('content', { required: true })}
-          /> */}
+          />
         </fieldset>
+      </div>
+      <div className="text-center md:text-right">
+        <button type="submit" className="btn btn-primary">
+          Save and Publish
+        </button>
       </div>
     </form>
   );
