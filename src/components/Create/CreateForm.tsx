20ae4b86b079b0536098f25b06df3390e7012288
Ezequiel Amin <ezequielamin@outlook.com>
2023-08-06 21:43:12 -0300

    feat: continue create form - found issue in markdown library

--- src/components/Create/CreateForm.tsx
+++ src/components/Create/CreateForm.tsx
@@ -1,16 +1,29 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-
 'use client';
 
+import { useForm } from 'react-hook-form';
+
+// import MDEditor from '@uiw/react-md-editor';
+// import rehypeSanitize from 'rehype-sanitize';
+
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 const CreateForm = () => {
+  const { register, handleSubmit: submitRHF } = useForm();
+
+  const handleSubmit = (data: unknown) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={submitRHF(handleSubmit)}>
       <div className="grid grid-cols-12 gap-3 my-5">
         <fieldset className="form-control col-span-12 md:col-span-6">
           <label htmlFor="title" className="text-lg">
             Post title
           </label>
-          <input id="title" className="input input-bordered w-full" />
+          <input
+            id="title"
+            className="input input-bordered w-full"
+            {...register('title', { required: true })}
+          />
         </fieldset>
         <fieldset className="form-control col-span-12 md:col-span-6">
           <label htmlFor="file" className="text-lg">
@@ -20,7 +33,23 @@ const CreateForm = () => {
             id="file"
             type="file"
             className="file-input input-bordered w-full"
+            {...register('file')}
           />
+        </fieldset>
+        <fieldset className="form-control col-span-12">
+          <label htmlFor="content" className="text-lg">
+            Content
+          </label>
+          {/* <MDEditor
+            id="content"
+            className="w-full"
+            value=""
+            onChange={() => {}}
+            previewOptions={{
+              rehypePlugins: [[rehypeSanitize]],
+            }}
+            // {...register('content', { required: true })}
+          /> */}
         </fieldset>
       </div>
     </form>
